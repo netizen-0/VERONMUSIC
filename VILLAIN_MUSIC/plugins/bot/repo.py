3cf5db2f5c8a2acb3d494cac1f576eada36e8bf3
netizen-0 <rishabhytpremium17@gmail.com>
2025-10-24 23:45:27 +0530

    Update repo.py

--- VILLAIN_MUSIC/plugins/bot/repo.py
+++ VILLAIN_MUSIC/plugins/bot/repo.py
@@ -13,7 +13,7 @@ start_txt = """
  
 ✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
 
-✰ || @iamakki001 ||
+✰ || @TFW_FOUNDER ||
  
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
  
@@ -29,14 +29,14 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔ᴅᴅ ᴍᴇ 𝗠ᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/oldskoolgc"),
-          InlineKeyboardButton("ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈", url="https://t.me/iamakki001"),
+          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/ATTACHME9T"),
+          InlineKeyboardButton("𝐕ᴇʀᴏɴ • ꭗ‌ • 𝐀ℓσɴє ❰ #𝐓𝐅Ꮗ ❱", url="https://t.me/tfw_founder"),
           ],
                [
-                InlineKeyboardButton("˹ᴠɪʟʟᴀɪɴ ꭙ ꜱᴜᴘᴘᴏʀᴛ˼", url=f"https://t.me/iamvillain77"),
+                InlineKeyboardButton("˹ᴠєʀσɴ • вσтѕ˼", url=f"https://t.me/Veron_bots"),
 ],
 [
-InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/radha_music_bot"),
+InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/VERONxMUSIC_BOT "),
 
         ]]
     
