d6fbdba86cd7deb10c5038f4fdfc7bba3e2d2e88
netizen-0 <rishabhytpremium17@gmail.com>
2025-12-25 18:08:48 +0530

    Update welcome.py

--- VILLAIN_MUSIC/plugins/tools/welcome.py
+++ VILLAIN_MUSIC/plugins/tools/welcome.py
@@ -166,10 +166,10 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 caption=f"""
 **⎊─────☵ ᴡᴇʟᴄᴏᴍᴇ ☵─────⎊**
 
-**☉ ɴᴀᴍᴇ ⧽** {user.mention}
-**☉ ɪᴅ ⧽** `{user.id}`
-**☉ ᴜ_ɴᴀᴍᴇ ⧽** @{user.username}
-**☉ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ⧽** {count}
+** ☉ ɴᴀᴍᴇ ⧽** {user.mention}
+** ☉ ɪᴅ ⧽** `{user.id}`
+** ☉ ᴜ_ɴᴀᴍᴇ ⧽** @{user.username}
+** ☉ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ⧽** {count}
 
 **⎉──────▢✭ 侖 ✭▢──────⎉**
 """,
