12121931a44a0787f585d1a24c70297554abf590
netizen-0 <rishabhytpremium17@gmail.com>
2025-12-25 17:28:41 +0530

    Update ping.py

--- VILLAIN_MUSIC/plugins/tools/ping.py
+++ VILLAIN_MUSIC/plugins/tools/ping.py
@@ -15,7 +15,7 @@ import asyncio
 @language
 async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
-        photo=PING_IMG_URL,
+        photo= "https://files.catbox.moe/jk10gh.jpg" ,
         caption=_["ping_1"].format(app.mention),
     )
     start = datetime.now()
